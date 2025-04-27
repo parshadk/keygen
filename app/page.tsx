66745c218026daa815ad8aaa8a91f07e6c1f7e2b
parshadk <parshadk04@gmail.com>
2025-04-27 20:22:33 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Walleten from "@/components/Walleten";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="p-4 m-4">
+      <Walleten />
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
         <div className="w-full h-16 bg-primary/50 backdrop-blur-sm  flex justify-center border-b border-primary/50 rounded-lg shadow-md shadow-primary/50 dark:bg-primary/50 dark:backdrop-blur-sm dark:border-primary/50 dark:shadow-primary/50"> 
             
             <div className="flex items-center justify-between w-full ">
-                <h1 className="text-2xl font-bold text-primary/80 p-4">Wallet Key Geneartor App</h1>
+                <h1 className="text-2xl font-bold text-primary/80 p-4">K3YG3N</h1>
             </div>
             <div className="flex items-center justify-end p-4 w-full">
                 <ModeToggle/>
